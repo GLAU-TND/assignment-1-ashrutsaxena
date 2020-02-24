5bb670057bbecea82e08d42e7f33d67c974dfaeb
ashrut saxena <ashrut.saxena_cs18@gla.ac.in>
2020-02-24 21:28:52 +0700

    person

---
+++ src/linkedlist/Person.java
@@ -0,0 +1,49 @@
+package linkedlist;
+
+import java.util.linkedlist;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private linkedlist<String> contacts;
+    private String email;
+
+    public Person(String firstName, String lastName, linkedlist<String> contacts, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contacts = contacts;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public linkedlist<String> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(linkedlist<String> contacts) {
+        this.contacts = contacts;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
